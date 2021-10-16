237586f96b97dfafa7fb7176cd993d6eab1bcb94
lleotraas <lleotraas@gmail.com>
2021-10-16 18:33:53 +0200

    Image picture, username and user email is show on the navigation drawer

--- app/src/main/java/com/openclassrooms/p7/go4lunch/MainActivity.java
+++ app/src/main/java/com/openclassrooms/p7/go4lunch/MainActivity.java
@@ -1,48 +1,46 @@
 package com.openclassrooms.p7.go4lunch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.material.navigation.NavigationView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 import com.openclassrooms.p7.go4lunch.databinding.ActivityMainBinding;
+import com.openclassrooms.p7.go4lunch.databinding.NavigationDrawerHeaderBinding;
 import com.openclassrooms.p7.go4lunch.manager.UserManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding mBinding;
+    private NavigationDrawerHeaderBinding mHeaderBinding;
     private final UserManager mUserManager = UserManager.getInstance();
+    private TextView email;
+    private TextView username;
+    private ImageView userPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.ConfigureBinding();
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-
-        setSupportActionBar(toolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mBinding.activityMainDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mBinding.activityMainDrawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-
+        this.configureNavigationDrawer();
         this.startSignActivity();
         this.configureViewPager();
         this.configureListeners();
+        this.updateHeader();
     }
-
-
 
     @Override
     public void onBackPressed() {
@@ -57,6 +55,18 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mBinding.getRoot();
         setContentView(view);
+    }
+
+    private void configureNavigationDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mBinding.activityMainDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mBinding.activityMainDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        final View parentView = mBinding.activityMainNavigationView.getHeaderView(0);
+        email = parentView.findViewById(R.id.header_email_adress_tv);
+        username = parentView.findViewById(R.id.header_username_tv);
+        userPicture = parentView.findViewById(R.id.header_user_image_img);
     }
 
     private void configureViewPager() {
@@ -92,5 +102,28 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void updateHeader() {
+        if (mUserManager.isCurrentUserLogged()) {
+            FirebaseUser user = mUserManager.getCurrentUser();
+
+            if (user.getPhotoUrl() != null) {
+                setUserPicture(user.getPhotoUrl());
+            }
+            setTextUserData(user);
+        }
+    }
+
+    private void setUserPicture(Uri photoUrl) {
+        Glide.with(this)
+                .load(photoUrl)
+                .apply(RequestOptions.circleCropTransform())
+                .into(userPicture);
+    }
+
+    private void setTextUserData(FirebaseUser user) {
+        username.setText(user.getDisplayName());
+        email.setText(user.getEmail());
     }
 }
